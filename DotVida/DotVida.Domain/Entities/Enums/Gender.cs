cbccf35f353a6e29323364cf7a9bf8c73fe0c682
Roberto Carlos <betocarlos00@hotmail.com>
2022-07-07 13:43:46 -0300

    estrutura dd

---
+++ DotVida/DotVida.Domain/Entities/Enums/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotVida.Domain.Entities.Enums
+{
+    public enum Gender
+    {
+        Masculino = 1,
+        Feminino = 2,
+        Outros = 3
+    }
+}
